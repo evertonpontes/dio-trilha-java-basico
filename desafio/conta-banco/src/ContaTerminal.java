ac326c051cc46325690af6bbaeb7dc733d036002
Everton <evertonpontesp@gmail.com>
2024-05-06 15:44:49 -0300

    edit README

--- desafio/conta-banco/src/ContaTerminal.java
+++ desafio/conta-banco/src/ContaTerminal.java
@@ -3,23 +3,34 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner myObj = new Scanner(System.in);
-        System.out.print("Por favor, digite o número da Conta e.g.(1021)> ");
+
+        System.out.printf("+-------------------------------+%n");
+        System.out.printf("|          Conta Banco          |%n");
+        System.out.printf("+-------------------------------+%n");
+
+        System.out.println("Por favor, digite o número da Conta e.g.(1021)");
 
         int numero = Integer.valueOf(myObj.nextLine());
-        
-        System.out.print("Por favor, digite o número da Agência e.g.(067-8)> ");
+
+        System.out.println("Por favor, digite o número da Agência e.g.(067-8)");
 
         String agencia = myObj.nextLine();
 
-        System.out.print("Por favor, digite o seu Nome> ");
+        System.out.println("Por favor, digite o seu Nome");
 
         String nomeCliente = myObj.nextLine();
 
-        System.out.print("Por favor, digite o seu Saldo> ");
+        System.out.println("Por favor, digite o seu Saldo");
 
         double saldo = Double.valueOf(myObj.nextLine());
 
-        System.out.print("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+        System.out.printf("+-------------------------------+%n");
+        System.out.printf("|           Resultado           |%n");
+        System.out.printf("+-------------------------------+%n");
+
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s já está disponível para saque.",
+                nomeCliente, agencia, numero, saldo, "%n");
 
     }
 }
