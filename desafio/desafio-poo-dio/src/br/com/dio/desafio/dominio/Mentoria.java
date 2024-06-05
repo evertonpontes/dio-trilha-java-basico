2a35a02d811c651f1fcf265c64c3015588ba6a86
Everton <evertonpontesp@gmail.com>
2024-06-05 20:10:33 -0300

    feat: add Conteudo class

--- desafio/desafio-poo-dio/src/br/com/dio/desafio/dominio/Mentoria.java
+++ desafio/desafio-poo-dio/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -1,4 +1,34 @@
 package br.com.dio.desafio.dominio;
 
-public class Mentoria {
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo{
+    private LocalDate data;
+
+    public Mentoria(String titulo, String descricao, LocalDate data) {
+        super(titulo, descricao);
+        this.data = data;
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\''+
+                ", data=" + data +
+                '}';
+    }
 }
